ec4f40188b3aec80b74b5d0958328b97541d8d05
Jacob V. Gardner <jacob.gardner@garmin.com>
2012-11-30 20:57:53 -0600

    timeline

--- src/timeline.py
+++ src/timeline.py
@@ -1,4 +1,36 @@
 
-class Timeline(object):
+class TrackObject(object):
     def __init__(self):
         pass
+
+class Track(object):
+    '''
+    Track is a container object 
+    '''
+    life = 0
+    death = float('Inf')
+
+    def __init__(self):
+        pass
+
+
+class Timeline(object):
+    tracks = []
+    def __init__(self):
+        pass
+
+    def add(self, obj):
+        t = Track(obj)
+        obj.track = t
+        tracks += [t]
+
+
+timeline.add(self, Ship(states))
+
+timeline = Timeline()
+
+ship = t.add(Ship)
+
+ship.move()
+
+

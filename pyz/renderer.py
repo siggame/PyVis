8ce0440e974017a40eda319beb46b0c38aefc2c1
Jacob Gardner <jacob.v.gardner@gmail.com>
2013-08-03 16:24:19 -0500

    ARC works filled and otherwise now.
     - Had to use indexed list because non indexed list was running into a
     	pyglet bug (https://code.google.com/p/pyglet/issues/detail?id=619)

--- pyz/renderer.py
+++ pyz/renderer.py
@@ -6,6 +6,7 @@ from copy import copy
 from pyglet import clock
 from pyglet import gl
 from pyglet import graphics
+import math
 
 class Drawable(object):
     x = property(
@@ -25,7 +26,6 @@ class Drawable(object):
         lambda self: self._height,
         lambda self, width: self.transform(scale=(self._width, height))
         )
-
 
 
 class Primitive(Drawable):
@@ -144,6 +144,152 @@ class Renderer(object):
         self.frame.draw()
         self.fps_display.draw()
 
+    class Arc(Primitive):
+        '''
+        This class creates an arc primitive for which circles and pies can be created.
+
+
+        :param x: The x offset (from the left side of the screen) to the arc center
+        :type x: float
+
+        :param y: The y offset (from the bottom of the screen) to the arc center
+        :type y: float
+
+        :param radius: The radius of the arc
+        :type radius: float
+
+        :param points: The number of vertices to make up the arc
+        :type points: int
+
+        '''
+
+        def __init__(self, x, y, radius, points=40, texture=None, group=None, color=None,
+            filled=True, loop=True, start=0, end=360, **kwargs):
+            super(Renderer.Arc, self).__init__(**kwargs)
+
+            self._x, self._y = x, y
+            self._radius = radius
+            self._points = points
+            self._start, self._end = start, end
+            self._filled = filled
+            self._loop = loop
+
+            if not color:
+                color = self.renderer.fg_color
+
+            data = [
+                ('v2f', (0,) * points * 2),
+                ('c4f', (color) * points)
+            ]
+
+            '''
+            if texture:
+                data += [
+                    ('t2f',
+                        (0, 0,
+                         1, 0,
+                         1, 1,
+                         0, 1))
+                ]
+                group = graphics.TextureGroup(texture, parent=group)
+            '''
+
+            indices = []
+            if filled:
+                # TODO: Change this to gl.GL_TRIANGLE_FAN when pyglet has fixed its
+                # allocator issues around TRIANGLE_FANs and LINE_LOOPs
+
+                mode = gl.GL_TRIANGLES
+
+                for p in range(points - 1):
+                    indices += [0, p, p + 1]
+            else:
+
+                for p in range(points - 1):
+                    indices += [p, p + 1]
+
+                if loop:
+                    indices = [0, 1] + indices + [points - 1, 0]
+
+                mode = gl.GL_LINES
+
+
+            self.vertex_list = self.renderer.frame.add_indexed(points, mode,
+                group, indices, *data)
+
+            self.transform()
+
+        def transform(self, translate=None, scale=None, rotate=None,
+                start=None, end=None):
+            '''
+            This transform method actually modifies the vertex lists to update the positions of the polygons.  This is most efficient when in unit-mode where only one unit is moving at a time.
+
+            This is because at this point very few objects are moving per frame. So all objects that have not moved take up zero cpu time.
+
+            This may not be as efficient when applied to normal, condensed, or liquid mode because far more polygons will be moving at the same time.  It may be better to draw each vertex list separately after having gone through a matrix transformation.
+
+            As a final note, it may be best to move these primitives into a class built in cython so that python doesn't have to deal with all this bullshit processing.
+
+            :param translate: The new position.
+            :type translate: 2-tuple of float or int
+
+            :param scale: The new scale.
+            :type scale: 2-tuple of float or int
+            '''
+            if translate:
+                self._x, self._y = translate
+            if scale:
+                self._radius = scale
+
+            if start:
+                self._start = start
+
+            if end:
+                self._end = end
+
+            start = math.radians(self._start)
+            end = math.radians(self._end)
+
+            if self._filled or self._loop:
+                point_count = self._points - 1
+            else:
+                point_count = self._points
+
+            interval = (end - start) / (point_count - 1)
+
+            points = []
+
+            t = start
+            for p in range(point_count):
+                x = (self._x + self.off_x) + self._radius * math.cos(t)
+                y = (self._y + self.off_y) + self._radius * math.sin(t)
+                points += [x, y]
+                t += interval
+
+            if self._filled or self._loop:
+
+                if not self._filled and self._start % 360 == self._end % 360:
+                    points = points[0:2] + points
+                else:
+                    points = [self._x + self.off_x, self._y + self.off_y] + points
+
+            self.vertex_list.vertices[:] = points
+
+        radius = property(
+            lambda self: self._radius,
+            lambda self, radius: self.transform(radius=radius)
+            )
+
+        start = property(
+            lambda self: self._start,
+            lambda self, start: self.transform(start=start)
+            )
+
+        end = property(
+            lambda self: self._end,
+            lambda self, end: self.transform(end=end)
+            )
+
 
     class Rectangle(Primitive):
         '''
@@ -179,18 +325,16 @@ class Renderer(object):
 
             super(Renderer.Rectangle, self).__init__(**kwargs)
 
-            self._x = x
-            self._y = y
-            self._width = width
-            self._height = height
+            self._x, self._y = x, y
+            self._width, self._height = width, height
 
             if not color:
                 color = self.renderer.fg_color
 
-            data = [('v2f', (0,) * 8),
-                ('c4f',
-                    (color) * 4)]
-
+            data = [
+                ('v2f', (0,) * 8),
+                ('c4f', (color) * 4)
+            ]
 
             if texture:
                 data += [
@@ -198,20 +342,23 @@ class Renderer(object):
                         (0, 0,
                          1, 0,
                          1, 1,
-                         0, 1))]
+                         0, 1))
+                ]
                 group = graphics.TextureGroup(texture, parent=group)
 
             if filled:
                 # TODO: Change this to gl.GL_TRIANGLE_FAN when pyglet has fixed its
                 # allocator issues around TRIANGLE_FANs and LINE_LOOPs
-                mode = gl.GL_TRIANGLE_FAN
+                mode = gl.GL_TRIANGLES
+                indices = [0, 1, 2, 0, 2, 3]
             else:
-                mode = gl.GL_LINE_LOOP
+                mode = gl.GL_LINES
+                indices = [0, 1, 1, 2, 2, 3, 3, 0]
 
-            self.vertex_list = self.renderer.frame.add(4, mode, group,
-                *data)
+            self.vertex_list = self.renderer.frame.add_indexed(4, mode, group,
+                indices, *data)
 
-            self.transform(translate=(x, y))
+            self.transform()
 
         def transform(self, translate=None, scale=None, rotate=None):
             '''
